31e93f4e92d658abcebef8e1a1e39cdb32de048e
Mounika Gurram <mounikagurram@Rajeevs-MacBook.local>
2020-02-14 14:41:46 -0800

    react basics along state basics

---
+++ react_complete-guide/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+state = {
+  person: [
+    {name: 'mouni', age: 27},
+    {name: 'rajee', age: 31},
+    {name: 'rajmo', age: 1}
+  ]
+}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React</h1>
+        <button>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies: Video Games</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React' ) )
+  } 
+  
+}
+
+export default App;
---
+++ react_complete-guide/src/Person/Person.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//here no need of importing component as we rae not using any class which extends component
+
+
+const person = (props) => {
+  return (
+    <div>
+      <p>I'am {props.name} and I am {props.age} years old!</p> 
+      <p>{props.children}</p>
+    </div>
+      
+  )
+}
+
+//this jsx //meed to compile into React.createElement so need to import React
+
+export default person;
