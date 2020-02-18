a8c458fb11e08fcd9b3df3e4ce4d9b9f43facd9d
mkalluri92 <mkalluri92@gmail.com>
2020-02-18 13:35:51 -0800

    using radium for pseudo selectors

--- react_complete-guide/src/App.js
+++ react_complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -52,7 +53,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -71,6 +76,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'balck'
+      }
     }
 
     let classes = [];
@@ -142,4 +151,4 @@ const app = (props) => {
 } 
 
 */
-export default App;
+export default Radium(App);
