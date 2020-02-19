e5e25d229e8ff8dd55f179557455a0235030113b
mkalluri92 <mkalluri92@gmail.com>
2020-02-18 17:02:32 -0800

    adding media queries with help of radium and StyleRoot

--- react_complete-guide/src/Person/Person.js
+++ react_complete-guide/src/Person/Person.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
 //here no need of importing component as we rae not using any class which extends component
 
 
 const person = (props) => {
+
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
   return (
-    <div className="Person"> 
+    <div className="Person" style= {style}> 
       <p onClick= {props.click} >I'am {props.name} and I am {props.age} years old!</p> 
       <p>{props.children}</p>
       <input type='text' onChange= {props.changed} value= {props.name}></input>
@@ -17,4 +24,4 @@ const person = (props) => {
 
 //this jsx //meed to compile into React.createElement so need to import React
 
-export default person;
+export default Radium(person);
