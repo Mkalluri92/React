a1139f517502ce97db0e6b8a675e318195afb029
mkalluri92 <mkalluri92@gmail.com>
2020-02-29 16:05:25 -0800

    hoc

--- react_complete-guide/src/containers/App.js
+++ react_complete-guide/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/withClass';
 
 
 
@@ -96,7 +97,7 @@ class App extends Component {
     }
 
     return (
-        <div className={classes.App}>
+        <WithClass classes={classes.App}>
           <button onClick = {() => {
             const show = this.state.cockpitShow
             this.setState({cockpitShow: !show})
@@ -109,7 +110,7 @@ class App extends Component {
             clicked= {this.togglePersonsHandler}>
           </Cockpit> : null }
           {persons}
-        </div>
+        </WithClass>
       )
   };
 }
